1b965dc7ed4263289f91782265a8e3a95a873a4c
Jason <jasonandy@hotmail.com>
2019-05-05 17:41:47 +0800

    fix: optz webapp

---
+++ skeleton-webapp/src/main/java/cn/ucaner/skeleton/webapp/controller/data/Zhifu.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * ~ Copyright (c) 2018 [jasonandy@hotmail.com | https://github.com/Jasonandy] *
+ * ~                                                                           *
+ * ~ Licensed under the Apache License, Version 2.0 (the "License”);           *
+ * ~ you may not use this file except in compliance with the License.          *
+ * ~ You may obtain a copy of the License at                                   *
+ * ~                                                                           *
+ * ~    http://www.apache.org/licenses/LICENSE-2.0                             *
+ * ~                                                                           *
+ * ~ Unless required by applicable law or agreed to in writing, software       *
+ * ~ distributed under the License is distributed on an "AS IS" BASIS,         *
+ * ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * ~ See the License for the specific language governing permissions and       *
+ * ~ limitations under the License.                                            *
+ ******************************************************************************/
+package cn.ucaner.skeleton.webapp.controller.data;
+
+import cn.ucaner.skeleton.common.vo.RespBody;
+import com.alibaba.fastjson.JSON;
+import com.github.abel533.echarts.Label;
+import com.github.abel533.echarts.Option;
+import com.github.abel533.echarts.code.Orient;
+import com.github.abel533.echarts.code.Tool;
+import com.github.abel533.echarts.code.X;
+import com.github.abel533.echarts.code.Y;
+import com.github.abel533.echarts.data.PieData;
+import com.github.abel533.echarts.series.Pie;
+import com.github.abel533.echarts.style.ItemStyle;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @projectName：Skeleton-X
+ * @Package：cn.ucaner.skeleton.webapp.controller.data
+ * @Description： <p> TODO </p>
+ * @Author： - Jason
+ * @CreatTime：2019/5/5 - 17:27
+ * @Modify By：
+ * @ModifyTime： 2019/5/5
+ * @Modify marker：
+ */
+@Controller
+@RequestMapping(value="/zhifu")
+public class Zhifu {
+
+
+    /**
+     * http://echarts.baidu.com/doc/example/pie6.html
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/index")
+    public RespBody index(){
+        RespBody respBody = new RespBody();
+        ItemStyle dataStyle = new ItemStyle();
+        dataStyle.normal().label(new Label().show(false)).labelLine().show(false);
+
+        ItemStyle placeHolderStyle = new ItemStyle();
+        placeHolderStyle.normal().color("rgba(0,0,0,0)").label(new Label().show(false)).labelLine().show(false);
+        placeHolderStyle.emphasis().color("rgba(0,0,0,0)");
+
+        Option option = new Option();
+        option.title().text("你幸福吗？")
+                .subtext("From ExcelHome")
+                .sublink("http://e.weibo.com/1341556070/AhQXtjbqh")
+                .x(X.center)
+                .y(Y.center)
+                .itemGap(20)
+                .textStyle().color("rgba(30,144,255,0.8)")
+                .fontFamily("微软雅黑")
+                .fontSize(35)
+                .fontWeight("bolder");
+        option.tooltip().show(true).formatter("{a} <br/>{b} : {c} ({d}%)");
+        option.legend().orient(Orient.vertical)
+                .x("(function(){return document.getElementById('main').offsetWidth / 2;})()")
+                .y(56)
+                .itemGap(12)
+                .data("68%的人表示过的不错", "29%的人表示生活压力很大", "3%的人表示“我姓曾”");
+        option.toolbox().show(true).feature(Tool.mark, Tool.dataView, Tool.restore, Tool.saveAsImage);
+
+        Pie p1 = new Pie("1");
+        p1.clockWise(false).radius(125, 150).itemStyle(dataStyle)
+                .data(new PieData("68%的人表示过的不错", 68), new PieData("invisible", 32));
+
+        Pie p2 = new Pie("2");
+        p2.clockWise(false).radius(100, 125).itemStyle(dataStyle)
+                .data(new PieData("29%的人表示生活压力很大", 29), new PieData("invisible", 71));
+
+        Pie p3 = new Pie("3");
+        p3.clockWise(false).radius(75, 100).itemStyle(dataStyle)
+                .data(new PieData("3%的人表示“我姓曾”", 3), new PieData("invisible", 97));
+        option.series(p1, p2, p3);
+        respBody.addOK(JSON.toJSONString(option),"数据渲染成功!");
+        return respBody;
+    }
+
+}
