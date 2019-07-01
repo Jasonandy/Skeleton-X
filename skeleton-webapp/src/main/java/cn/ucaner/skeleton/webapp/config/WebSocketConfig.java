4aefbfadbe727fb102fd7734fea155f22eb12d85
Jason <jasonandy@hotmail.com>
2019-07-01 16:27:19 +0800

    feat: add websocket

---
+++ skeleton-webapp/src/main/java/cn/ucaner/skeleton/webapp/config/WebSocketConfig.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * ~ Copyright (c) 2018 [jasonandy@hotmail.com | https://github.com/Jasonandy] *
+ * ~                                                                           *
+ * ~ Licensed under the Apache License, Version 2.0 (the "License”);           *
+ * ~ you may not use this file except in compliance with the License.          *
+ * ~ You may obtain a copy of the License at                                   *
+ * ~                                                                           *
+ * ~    http://www.apache.org/licenses/LICENSE-2.0                             *
+ * ~                                                                           *
+ * ~ Unless required by applicable law or agreed to in writing, software       *
+ * ~ distributed under the License is distributed on an "AS IS" BASIS,         *
+ * ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ * ~ See the License for the specific language governing permissions and       *
+ * ~ limitations under the License.                                            *
+ ******************************************************************************/
+package cn.ucaner.skeleton.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @projectName：Skeleton-X
+ * @Package：cn.ucaner.skeleton.service.framework.config.ws
+ * @Description： <p> WebSocketConfig  </p>
+ * @Author： - Jason
+ * @CreatTime：2019/7/1 - 14:43
+ * @Modify By：
+ * @ModifyTime： 2019/7/1
+ * @Modify marker：
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
